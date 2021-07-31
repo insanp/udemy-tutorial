74a75326dc7583a25434b5554f6c088c087ba12d
Insan <insan.putranda@gmail.com>
2021-07-31 20:50:03 +0700

    Added project files

---
+++ Assets/Script Lessons/Section 2 - Number Wizard Basic/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] int max, min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        //guess = (max + min) / 2;
+        int guessMin = min + 1;
+        int guessMax = max;
+        if (guessMin > guessMax) guessMin = guessMax;
+        if (guessMax < guessMin) guessMax = guessMin;
+
+        guess = Random.Range(guessMin, guessMax);
+        guessText.text = guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+}
---
+++ Assets/Script Lessons/Section 3 - Text101/Scripts/AdventureGame.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    string[] daysOfTheWeek = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+        /*
+        int index = -1;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            index = 0;
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            index = 1;
+        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            index = 2;
+        }
+
+        if (Input.anyKeyDown && index >= 0 && index < nextStates.Length)
+        {
+            state = nextStates[index];
+        }
+        */
+        textComponent.text = state.GetStateStory();
+    }
+}
